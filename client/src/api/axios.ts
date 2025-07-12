53a9bdf543b29a66f15d7878660d2cb64f02085a
zubayerforazi <forazizubayer@gmail.com>
2025-07-12 11:42:14 +0600

    updated axios

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -6,25 +6,25 @@ const api = axios.create({
   withCredentials: true,
 });
 
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        await api.get("/refresh-token");
-        return api(originalRequest);
-      } catch (refreshError) {
-        toast.error("Session expired. Please log in again.");
-        window.location.href = "/login";
-        return Promise.reject(refreshError);
-      }
-    }
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       try {
+//         await api.get("/refresh-token");
+//         return api(originalRequest);
+//       } catch (refreshError) {
+//         toast.error("Session expired. Please log in again.");
+//         window.location.href = "/login";
+//         return Promise.reject(refreshError);
+//       }
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
