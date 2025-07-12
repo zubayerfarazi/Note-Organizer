8969fa59a7f6dff60740986908c0f92be3d074d5
zubayerforazi <forazizubayer@gmail.com>
2025-07-12 11:43:05 +0600

    build error

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
